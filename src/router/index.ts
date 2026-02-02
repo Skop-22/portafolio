d0caf7ec99c526e2199e316a18da9b9ccfa1aba1
skop <skopflores@gmail.com>
2026-02-02 07:47:44 -0600

    configuración tailwindcss v4

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,4 @@
 import { createRouter, createWebHashHistory } from "vue-router";
-import HomeView from "../views/HomeView.vue";
 
 const router = createRouter({
   history: createWebHashHistory(),
@@ -7,12 +6,12 @@ const router = createRouter({
     {
       path: "/",
       name: "home",
-      component: HomeView,
+      component: () => import("../views/HomeView.vue"),
     },
     {
-      path: "/about",
-      name: "about",
-      component: () => import("../views/AboutView.vue"),
+      path: "/sobre-mi",
+      name: "sobre.mi",
+      component: () => import("../views/SobreMiView.vue"),
     },
   ],
 });
